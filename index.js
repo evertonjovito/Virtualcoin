4b83a12d0fad6ef6339cde665b2d6017512a9ef8
evertonjovito <evertonjovitooi@gmail.com>
2022-03-06 11:35:26 -0300

    Transaction

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const sha256 = require ('crypto-js/sha256')
-const hash = sha256('Éverton').toString()
+
+const transaction = {
+    from: 'a',
+    to: 'b',
+    amount: 1
+}
+
+const str = JSON.stringify(transaction)
+
+const hash = sha256(str).toString()
 console.log(hash)
